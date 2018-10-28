691ea81784a0a7e7669cc4fd2f1f25d4f0aee03d
Chen Yang <chen@pporn.tv>
2018-10-28 13:27:35 -0500

    refactor navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,32 +4,73 @@ import NavBarItem from './NavBarItem';
 import '../css/bulma.min.css';
 import icon from '../images/icon.png';
 
-function NavBar() {
-  return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="http://server.io">
-          <img src={icon} height="28" alt="server.io" />
-        </a>
-      </div>
+class NavBar extends React.Component {
+  constructor() {
+    super();
 
-      <div className="navbar-menu">
-        <div className="navbar-start">
+    this.state = {
+      navBarStartItems: {
+        Home: {
+          name: 'Home',
+          url: 'http://server.io',
+        },
+        AriaNg: {
+          name: 'AriaNg',
+          url: 'http://aria2.server.io',
+        },
+        Jupyter: {
+          name: 'Jupyter',
+          url: 'http://jupyter.server.io',
+        },
+        MiniDLNA: {
+          name: 'MiniDLNA',
+          url: 'http://minidlna.server.io',
+        },
 
-          <NavBarItem name="Home" url="http://server.io" />
-          <NavBarItem name="AriaNg" url="http://aria2.server.io" />
-          <NavBarItem name="Jupyter" url="http://jupyter.server.io" />
-          <NavBarItem name="MiniDLNA" url="http://minidlna.server.io" />
+      },
 
+      navBarEndItems: {
+        About: {
+          name: 'About',
+          url: 'http://server.io',
+        },
+      },
+
+    };
+  }
+
+  render() {
+    const { navBarStartItems, navBarEndItems } = this.state;
+
+    return (
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="http://server.io">
+            <img src={icon} height="28" alt="server.io" />
+          </a>
         </div>
-      </div>
 
-      <div className="navbar-end">
-        <NavBarItem name="About" url="http://server.io" />
-      </div>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            {
+              Object.keys(navBarStartItems).map(
+                i => <NavBarItem name={navBarStartItems[i].name} url={navBarStartItems[i].url} />,
+              )
+            }
+          </div>
+        </div>
 
-    </nav>
-  );
+        <div className="navbar-end">
+          {
+            Object.keys(navBarEndItems).map(
+              i => <NavBarItem name={navBarEndItems[i].name} url={navBarEndItems[i].url} />,
+            )
+          }
+        </div>
+
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
