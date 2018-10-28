94a0b9b8f5a719b4b05dc7f51767cf20d736db4e
Chen Yang <chen@pporn.tv>
2018-10-28 16:48:16 -0500

    improve UI

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 function Title() {
   return (
-    <section className="section">
-      <div className="container has-text-centered">
-        <p className="title is-unselectable">
-          Home Server
-        </p>
+    <div className="hero is-dark is-fullheight-with-navbar is-bold">
+      <div className="hero-body">
+
+        <div className="container has-text-centered">
+          <h1 className="title is-unselectable">
+            Home Server
+          </h1>
+
+          <h2 className="subtitle is-unselectable">
+            Ubuntu Server 18.04
+          </h2>
+
+        </div>
+
       </div>
-    </section>
+    </div>
   );
 }
 
